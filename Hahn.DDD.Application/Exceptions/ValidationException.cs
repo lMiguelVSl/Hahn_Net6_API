4c8c5568e2bcac1cd5043706affea76b2c5d4773
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-29 21:12:28 -0500

    Validation exception class created

---
+++ Hahn.DDD.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+
+namespace Hahn.DDD.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("One or more error happened in the operation")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup =>  failureGroup.ToArray());
+        }
+
+        public Dictionary<string, string[]> Errors { get;}
+    }
+}
