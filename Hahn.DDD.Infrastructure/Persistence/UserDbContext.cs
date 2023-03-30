7aca13e946e11591eb25b7611a39f46b88448fa9
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-29 22:17:58 -0500

    Db context audit

---
+++ Hahn.DDD.Infrastructure/Persistence/UserDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Hahn.DDD.Domain;
+using Hahn.DDD.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Hahn.DDD.Infrastructure.Persistence
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        entry.Entity.CreateBy = "System";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        entry.Entity.LastModifiedBy = "System";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+}
