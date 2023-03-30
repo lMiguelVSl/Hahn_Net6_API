aa8b36972efc9a89f6b9f53f9c0ad2019249ee30
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-30 18:21:29 -0500

    Dependency injection infrastructure project and API

---
+++ Hahn.DDD.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Hahn.DDD.Application.Contracts.Persistence;
+using Hahn.DDD.Infrastructure.Persistence;
+using Hahn.DDD.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hahn.DDD.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<UserDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            );
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ Hahn.DDD.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Hahn.DDD.Application.Contracts.Persistence;
+using Hahn.DDD.Domain;
+using Hahn.DDD.Infrastructure.Persistence;
+
+namespace Hahn.DDD.Infrastructure.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(UserDbContext context) : base(context)
+        {
+        }
+    }
+}
