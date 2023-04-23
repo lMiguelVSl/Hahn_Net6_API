c737c414c0811aa0bd2ee77f69fbe9123b83921c
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-04-23 09:50:53 -0500

    security layer classes

---
+++ Hahn.DDD.Application/Models/Identity/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Hahn.DDD.Application.Models.Identity
+{
+    public class AuthRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
