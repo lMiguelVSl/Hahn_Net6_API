a771aba47f27510c475cbbe45251b5d9c5a7eda9
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-04-23 10:58:43 -0500

    Services regiatration

---
+++ Hahn.DDD.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Hahn.DDD.Application.Models.Identity;
+
+namespace Hahn.DDD.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+    }
+}
