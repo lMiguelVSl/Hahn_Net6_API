0ef1dfd08119508d0d50e13e7c4aba5d20039102
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-04-23 10:58:59 -0500

    login methos implementation

--- Hahn.DDD.Application/Models/Identity/RegistrationRequest.cs
+++ Hahn.DDD.Application/Models/Identity/RegistrationRequest.cs
@@ -2,8 +2,8 @@
 {
     public class RegistrationRequest
     {
-        public string Nombre { get; set; } = string.Empty;
-        public string Apellidos { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
